3e324c637685d34db43b31b1a9625c632a43d786
DaKern <daniel.kern92@gmail.com>
2011-10-13 12:27:41 +0200

    implement GUI, including DeviceFrame and TopologyFrame

---
+++ src/at/netcrawler/ui/InterfacePanel.java
@@ -0,0 +1,24 @@
+package at.netcrawler.ui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+@SuppressWarnings("serial")
+public class InterfacePanel extends JComponent{
+
+	JPanel panel = new JPanel();
+	
+	JLabel text = new JLabel("Hallo :D");
+	
+	public InterfacePanel() {
+		
+		panel.add(text);
+		setLayout(new BorderLayout());
+		add(panel);
+	}
+
+}
