2aba7db45fdb91d340f59e5f91a5a63fbc06cb1c
Andreas Stefl <stefl.andreas@gmail.com>
2011-11-29 09:41:33 +0100

    improves generic naming

--- src/at/netcrawler/network/connection/ConnectionGateway.java
+++ src/at/netcrawler/network/connection/ConnectionGateway.java
@@ -5,15 +5,15 @@ import java.io.IOException;
 import at.netcrawler.network.accessor.DeviceAccessor;
 
 
-public abstract class ConnectionGateway<DA extends DeviceAccessor, CS extends ConnectionSettings> {
+public abstract class ConnectionGateway<A extends DeviceAccessor, S extends ConnectionSettings> {
 	
 	public abstract Class<? extends DeviceConnection> getDeviceConnectionClass();
 	
-	public abstract Class<DA> getDeviceAccessorClass();
+	public abstract Class<A> getDeviceAccessorClass();
 	
-	public abstract Class<CS> getConnectionSettingsClass();
+	public abstract Class<S> getConnectionSettingsClass();
 	
-	public abstract DeviceConnection openConnection(DA accessor, CS settings)
+	public abstract DeviceConnection openConnection(A accessor, S settings)
 			throws IOException;
 	
 }
