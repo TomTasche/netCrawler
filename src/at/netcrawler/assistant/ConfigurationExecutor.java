a25d15fd9d7e53f5dce4666135a6d8e320a58c41
Thomas Taschauer <tomtasche@gmail.com>
2012-03-07 15:38:59 +0100

    create .jar and .jardesc for netcrawler and configuration assistant

---
+++ src/at/netcrawler/assistant/ConfigurationExecutor.java
@@ -0,0 +1,23 @@
+package at.netcrawler.assistant;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+import at.andiwand.library.component.JFrameUtil;
+import at.netcrawler.ui.graphical.main.BatchExecutor;
+
+public class ConfigurationExecutor {
+	
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			System.err.println("Couldn't apply system look and feel.");
+		}
+		
+		BatchExecutor executor = new BatchExecutor();
+		executor.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		JFrameUtil.centerFrame(executor);
+		executor.setVisible(true);
+	}
+}
